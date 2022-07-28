efffedbe9c557d67d3862d1500627ef1e2019b8f
Ivan Carreño <carreaguileraivan@gmail.com>
2022-07-28 11:35:06 -0300

    Utilizando Props

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // element.innerText = 'Hello React..'
 // const container = document.getElementById('root')
 // container.appendChild(element)
-
+//https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06
 import { getDefaultNormalizer } from '@testing-library/react'
 import React from 'react'
 import ReactDOM  from 'react-dom'
@@ -14,4 +14,10 @@ const container = document.getElementById('root')
 
 //ReactDOM.render(que, donde)
 
-ReactDOM.render(<Card />, container)
+ReactDOM.render(<Card 
+                title="Technique Guides"
+                description="Learn amazing street workout and calisthenics"
+                img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                leftColor="#A74CF2"
+                rightColor="#617BFB"
+                />, container)
