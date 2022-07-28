2805e1f2e6a4a08cd8823f3bc3825f1d559ddc08
Ivan Carreño <carreaguileraivan@gmail.com>
2022-07-27 22:28:48 -0300

    Iniciando Proyecto React APPFitness

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const element = document.createElement('h1')
+// element.innerText = 'Hello React..'
+// const container = document.getElementById('root')
+// container.appendChild(element)
+
+import { getDefaultNormalizer } from '@testing-library/react'
+import React from 'react'
+import ReactDOM  from 'react-dom'
+import Card from './components/Card'
+
+
+const container = document.getElementById('root')
+
+//ReactDOM.render(que, donde)
+
+ReactDOM.render(<Card />, container)
