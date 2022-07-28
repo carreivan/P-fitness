9842a8d311ed9405b9771a1ae6ae7e902cbe4835
Ivan Carreño <carreaguileraivan@gmail.com>
2022-07-27 23:02:00 -0300

    Adicionando estilos

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { getDefaultNormalizer } from '@testing-library/react'
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 const container = document.getElementById('root')
